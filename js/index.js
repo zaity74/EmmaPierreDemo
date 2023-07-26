6e65c76ad6fbb06179e3ac63e0a5e3b45cfac7b0
frederic zai <frederic.zai@gmail.com>
2023-07-26 12:34:05 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -2,15 +2,3 @@
 let arrowLeft = document.querySelector('.bi-arrow-left-circle');
 let arrowRight = document.querySelector('.bi-arrow-right-circle');
 
-// Slide container
-let slideContainer = document.querySelector('.slide-container');
-// Onclick event
-function slideRight() {
-    slideContainer.style.transform = 'translateX(-10%)';
-  }
-  function slideLeft() {
-    slideContainer.style.transform = 'translateX(10%)';
-  }
-
-  arrowRight.addEventListener('click',slideRight);
-  arrowLeft.addEventListener('click',slideLeft);
